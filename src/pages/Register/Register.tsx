bbea117ba20ed679746f8854a98d899379bdd68d
Damian Tymkowski <thedamianpl@gmail.com>
2021-03-21 20:28:14 +0100

    Add Register Form component to Register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import * as Styled from "./style.styles";
 
 const Register = () => {
-    return(
-        <>
-          <Header/>
-        </>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Styled.Container>
+        <RegisterForm />
+      </Styled.Container>
+    </>
+  );
+};
 
-export default Register
+export default Register;
