55b8de080eb0dc1acab8bc302ec928031c1dd72f
Damian Tymkowski <thedamianpl@gmail.com>
2021-03-22 15:57:41 +0100

    Add request to api & update errors handler

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,34 +1,75 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 interface initialValues {
   email: string;
   password: string;
+  repeatPassword: string;
 }
 
 const useForm = (initialValues: initialValues) => {
   const [inputs, setInputs] = useState(initialValues);
   const [errors, setErrors] = useState(initialValues);
   const [isFirst, setIsFirst] = useState(true);
+  const [currentElement, setCurrentElement] = useState("");
+  const [registerInfo, setRegisterInfo] = useState("");
 
-  const validate = (inputs: initialValues) => {
-    const errors = {
-      email: "",
-      password: "",
+  const validate = (inputs: initialValues, element: string) => {
+    const Errors = {
+      email: errors.email,
+      password: errors.password,
+      repeatPassword: errors.repeatPassword,
     };
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!regex.test(inputs.email.toLowerCase())) errors.email = "Invalid email";
-    if (inputs.email.length == 0) errors.email = "Email is empty";
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    if (
+      (!regexEmail.test(inputs.email.toLowerCase()) && element === "email") ||
+      (element == "Submit" && !regexEmail.test(inputs.email.toLowerCase()))
+    )
+      Errors.email = "Nieprawidłowy adres email";
+    else if (!regexEmail.test(inputs.email.toLowerCase()) && element != "email")
+      Errors.email = errors.email;
+    else Errors.email = "";
 
-    setErrors(errors);
+    if (
+      (!regexPassword.test(inputs.password) && element === "password") ||
+      (element == "Submit" && !regexPassword.test(inputs.password))
+    ) {
+      Errors.password =
+        "Hasło musi zawierać minimum 8 znaków, jedną dużą i małą literę i jedną cyfrę";
+    } else if (
+      !regexEmail.test(inputs.password.toLowerCase()) &&
+      element != "password"
+    )
+      Errors.password = errors.password;
+    else Errors.password = "";
+
+    if (inputs.repeatPassword !== inputs.password)
+      Errors.repeatPassword = "Hasła nie są takie same!";
+    else Errors.repeatPassword = "";
+
+    setErrors(Errors);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
-    validate(inputs);
+    validate(inputs, "Submit");
+    axios({
+      method: "post",
+      url: "http://localhost/nowydom_server/registrationLogging.php",
+      data: {
+        email: inputs.email,
+        password: inputs.password,
+        action: "registration",
+      },
+    }).then((response) => {
+      setRegisterInfo(response.status.toString());
+      console.log(response);
+    });
     e ? e.preventDefault() : console.log(inputs);
   };
 
   useEffect(() => {
-    if (!isFirst) validate(inputs);
+    if (!isFirst) validate(inputs, currentElement);
   }, [inputs]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,6 +79,7 @@ const useForm = (initialValues: initialValues) => {
       [e.target.name]: e.target.value,
     }));
     setIsFirst(false);
+    setCurrentElement(e.target.name);
   };
 
   return {
@@ -45,6 +87,7 @@ const useForm = (initialValues: initialValues) => {
     handleInputChange,
     inputs,
     errors,
+    registerInfo,
   };
 };
 
