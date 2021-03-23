636f2921bc63274ab915243f0807d47ac5e29434
Damian Tymkowski <thedamianpl@gmail.com>
2021-03-23 21:12:45 +0100

    Add new route '/logowanie' for Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/rejestracja" component={Register} />
+        <Route path="/logowanie" component={Login} />
       </Switch>
     </Router>
   );
