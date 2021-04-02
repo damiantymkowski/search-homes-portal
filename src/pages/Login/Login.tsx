fd109445dc5d019d9ebe02787b6453b6dc2550b7
Damian Tymkowski <thedamianpl@gmail.com>
2021-04-02 15:57:30 +0200

    Add container to Login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import * as Styled from "./style.styles";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import Footer from "../../components/Footer/Footer";
 
 const Login = () => {
   return (
     <>
       <Header />
       <Styled.Container>
-        <Styled.HeaderLogin>
-          <Styled.HeaderText>Logowanie</Styled.HeaderText>
-        </Styled.HeaderLogin>
-        <LoginForm />
+        <Styled.LoginBox>
+          <Styled.HeaderLogin>
+            <Styled.HeaderText>Logowanie</Styled.HeaderText>
+          </Styled.HeaderLogin>
+          <LoginForm />
+        </Styled.LoginBox>
+        <Footer />
       </Styled.Container>
     </>
   );
