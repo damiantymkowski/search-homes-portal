06b5233ab18d83b4e002ea1d7f7fe19b0e058af9
Damian Tymkowski <thedamianpl@gmail.com>
2021-04-02 15:58:36 +0200

    Add container to register component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import * as Styled from "./style.styles";
+import Footer from "../../components/Footer/Footer";
 
 const Register = () => {
   return (
     <>
       <Header />
       <Styled.Container>
-        <Styled.HeaderRegister>
-          <Styled.HeaderText>Zarejestruj się</Styled.HeaderText>
-        </Styled.HeaderRegister>
-        <RegisterForm />
+        <Styled.RegisterBox>
+          <Styled.HeaderRegister>
+            <Styled.HeaderText>Zarejestruj się</Styled.HeaderText>
+          </Styled.HeaderRegister>
+          <RegisterForm />
+        </Styled.RegisterBox>
+        <Footer />
       </Styled.Container>
     </>
   );
