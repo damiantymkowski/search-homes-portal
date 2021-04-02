55b9afcced1e58efb74586b1081c98c73b023a0a
Damian Tymkowski <thedamianpl@gmail.com>
2021-04-02 15:56:48 +0200

    Add new exact path for Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Homepage} />
         <Route path="/rejestracja" component={Register} />
         <Route path="/logowanie" component={Login} />
       </Switch>
